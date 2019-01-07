dcdf25adfda0ec09490ff3569a7d3a60fc067d87
Sean T <teo259@gmail.com>
2019-01-07 12:02:37 +0800

    Add and comment loginreminder mail method

--- App_Code/MailUtilities.cs
+++ App_Code/MailUtilities.cs
@@ -241,4 +241,46 @@ public class MailUtilities
         return x;
     }
 
+    /*
+    //for sending reminder to patient to login to portal
+    public int sendLoginReminder(string email, string name, string password)
+    {
+        SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+        int result = 1;
+
+        string body = "Hi " + name + ",<br/>This is a reminder to login to your <br/><br/>" + "This is your new MediCare account password:<br/><b>" + password + "</b><br/><br/>" + "Please login at the link below and change the password on your first login.<br/>" + " <a href=\"http://localhost:49947/Login/Login\">Login Here</a> ";
+
+        smtpClient.UseDefaultCredentials = false;
+        smtpClient.Credentials = new System.Net.NetworkCredential("aspmedicare2018@gmail.com", "Exact123");
+        smtpClient.EnableSsl = true;
+        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        MailMessage mail = new MailMessage();
+
+        //Setting From , To and CC
+        mail.From = new MailAddress("aspmedicare2018@gmail.com", "MediCare Portal");
+        mail.To.Add(new MailAddress(email));
+
+
+        //to set the contents of the email
+        mail.Subject = "Reset of MediCare password";
+        mail.Body = body;
+        mail.IsBodyHtml = true;
+
+        mail.BodyEncoding = System.Text.Encoding.UTF8;
+
+        try
+        {
+            smtpClient.Send(mail);
+
+        }
+        catch (SmtpException ex)
+        {
+            result = 0;
+        }
+
+        return result;
+
+    }
+    */
 }
