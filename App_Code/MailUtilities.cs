a01c4fdd9ec224d2433b1d47eca7e74460cc6720
Sean T <teo259@gmail.com>
2019-01-14 22:15:45 +0800

    Add profile changed email alerts

--- App_Code/MailUtilities.cs
+++ App_Code/MailUtilities.cs
@@ -369,6 +369,48 @@ public class MailUtilities
         return result;
     }
 
+
+    //for notice of security qn change
+    public int sendProfileChanged(string email, string name)
+    {
+        SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+        int result = 1;
+
+        string body = "Hi " + name + ",<br/><br/><b>Your MediCare account details has been changed.</b><br/><br/>" + "Please contact the clinic at 6458 9900 if your account has not been changed by you.<br/>";
+
+        smtpClient.UseDefaultCredentials = false;
+        smtpClient.Credentials = new System.Net.NetworkCredential("aspmedicare2018@gmail.com", "Exact123");
+        smtpClient.EnableSsl = true;
+        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        MailMessage mail = new MailMessage();
+
+        //Setting From , To and CC
+        mail.From = new MailAddress("aspmedicare2018@gmail.com", "MediCare Portal");
+        mail.To.Add(new MailAddress(email));
+
+
+        //to set the contents of the email
+        mail.Subject = "MediCare Account Security Alert";
+        mail.Body = body;
+        mail.IsBodyHtml = true;
+
+        mail.BodyEncoding = System.Text.Encoding.UTF8;
+
+        try
+        {
+            smtpClient.Send(mail);
+
+        }
+        catch (SmtpException ex)
+        {
+            Debug.Write(ex);
+            result = 0;
+        }
+
+        return result;
+    }
+
     /*
     //for sending reminder to patient to login to portal
     public int sendLoginReminder(string email, string name, string password)
